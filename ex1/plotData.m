699512e64e064ace97802b1d3f0d67b61606ea05
hieunh161 <hieunh161@gmail.com>
2020-05-10 01:36:29 +0900

    add compute cost

--- ex1/plotData.m
+++ ex1/plotData.m
@@ -21,5 +21,8 @@ figure; % open a new figure window
 
 
 % ============================================================
+plot(x,y,'rx', 'MarkerSize', 10)
+xlabel("Profit in $10,000s")
+ylabel("Population of City in 10,000s")
 
 end
